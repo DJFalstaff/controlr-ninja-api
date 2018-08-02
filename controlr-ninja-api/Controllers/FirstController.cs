cceab0155631f6f54a2b019ad58452f8cc934aa9
Main <djfalstaff@gmail.com>
2018-08-01 22:03:33 -0400

    second commit
    second commit to test branch logic

---
+++ controlr-ninja-api/Controllers/FirstController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace controlr_ninja_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstController : ControllerBase
+    {
+    }
+}
